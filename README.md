36619b7c2d6d2c8c5a46786c90f718c0ac2e301a
Lucas Garron <code@garron.net>
2021-10-15 22:22:44 -0700

    Initial version.

---
+++ README.md
@@ -0,0 +1,7 @@
+## Build
+
+```shell
+npm install
+npm run build
+python3 -m http.server 8080
+```
---
+++ src/js/@vzome/react-vzome/index.js
@@ -0,0 +1,3 @@
+import "regenerator-runtime/runtime";
+import vZome from "@vzome/react-vzome";
+export default vZome;
